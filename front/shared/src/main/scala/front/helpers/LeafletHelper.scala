e2aab0fe03c3ca9935dc7578d4e3ae857b2df6cb
Matt Langsenkamp <ml2513@rit.edu>
2023-07-31 22:22:31 -0400

    ok

--- front/shared/src/main/scala/front/helpers/LeafletHelper.scala
+++ front/shared/src/main/scala/front/helpers/LeafletHelper.scala
@@ -5,6 +5,8 @@ import hello.{Location, Locations}
 import typings.leaflet.mod as L
 import typings.leaflet.mod.{IconOptions, Icon_, Marker_}
 import tyrian.Html.div
+import tyrian.Html
+import tyrian.Html.*
 import tyrian.Cmd.Emit
 
 object LeafletHelper:
@@ -31,7 +33,9 @@ object LeafletHelper:
     //  println("dragged")
     //  Emit(Msg.NoOp)
     // )
+
     addLocations(m, model.locations)
+
 
   def addLocation(map: L.Map_, location: Location): L.Map_ =
     val p = L.popup(L.PopupOptions().setContent(setContent(location)))
@@ -50,36 +54,36 @@ object LeafletHelper:
     val url =
       f"https://www.google.com/maps/search/?api=1&query=${location.latitude},${location.longitude}"
     f"""<div class=\"inline-block break-words w-64\">
-    |       <div class=\"flex justify-around\">
-    |           <img src=\"https://picsum.photos/200/300\" class=\"max-h-60 rounded\">
-    |       </div>
-    |       <div class=\"text-lg w-64 flex font-semibold text-indigo-400 \">${location.name}</div>
-    |       <div class=\"\">${location.description}</div>
-    |       <div class=\"\">${location.address}</div>
-    |       <a href="$url" target="_blank">
-    |          <span
-    |            title="google"
-    |            class="text-lg w-64 flex font-semibold text-indigo-400 hover:text-indigo-200"
-    |          >
-    |            Google maps
-    |            <svg
-    |              xmlns="http://www.w3.org/2000/svg"
-    |              fill="none"
-    |              stroke="currentColor"
-    |              viewBox="0 0 24 24"
-    |              width="15"
-    |              height="15"
-    |            >
-    |              <path
-    |                stroke-linecap="round"
-    |                stroke-linejoin="round"
-    |                stroke-width="2"
-    |                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-    |              />
-    |            </svg>
-    |          </span>
-    |        </a>
-    |   </div>""".stripMargin
+       |       <div class=\"flex justify-around\">
+       |           <img src=\"https://picsum.photos/200/300\" class=\"max-h-60 rounded\">
+       |       </div>
+       |       <div class=\"text-lg w-64 flex font-semibold text-indigo-400 \">${location.name}</div>
+       |       <div class=\"\">${location.description}</div>
+       |       <div class=\"\">${location.address}</div>
+       |       <a href="$url" target="_blank">
+       |          <span
+       |            title="google"
+       |            class="text-lg w-64 flex font-semibold text-indigo-400 hover:text-indigo-200"
+       |          >
+       |            Google maps
+       |            <svg
+       |              xmlns="http://www.w3.org/2000/svg"
+       |              fill="none"
+       |              stroke="currentColor"
+       |              viewBox="0 0 24 24"
+       |              width="15"
+       |              height="15"
+       |            >
+       |              <path
+       |                stroke-linecap="round"
+       |                stroke-linejoin="round"
+       |                stroke-width="2"
+       |                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+       |              />
+       |            </svg>
+       |          </span>
+       |        </a>
+       |   </div>""".stripMargin
 
   def newLocationSelectionMarker(map: L.Map_): L.Marker_[Any] =
 
