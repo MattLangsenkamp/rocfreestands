aa5487bad1830ae021eaec423b7bc8f306b718ce
Matt Langsenkamp <ml2513@rit.edu>
2023-08-02 00:32:04 -0400

    ok

---
+++ web/main.js
@@ -0,0 +1,8 @@
+//import './style.css'
+//import './output.css'
+//import 'scalajs:main.js'
+
+import "./style.css";
+import { TyrianApp } from 'scalajs:main.js';
+
+TyrianApp.launch("myapp");
---
+++ web/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
+
+export default defineConfig({
+  plugins: [scalaJSPlugin({
+    cwd: '..',
+    projectID: 'frontJS'
+  })],
+});
